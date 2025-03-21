c81388c93e0de6e9c366c2a4180a04d813e57330
Steven Haesler <hasv@zhaw.ch>
2025-03-21 17:48:55 +0100

    remove unused import

--- src/main/java/ch/zhaw/init/ehr/ehrbackend/EhrbackendApplication.java
+++ src/main/java/ch/zhaw/init/ehr/ehrbackend/EhrbackendApplication.java
@@ -3,8 +3,6 @@ package ch.zhaw.init.ehr.ehrbackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import ch.zhaw.init.ehr.ehrbackend.restservice.Greeting;
-
 
 @SpringBootApplication
 public class EhrbackendApplication {
