5b0b50e1e50237607b9024a4d9f398a8245506c0
Steven Haesler <hasv@zhaw.ch>
2025-04-23 10:10:36 +0200

    remove token log

--- ehr-frontend-next/app/services/doctors_service.tsx
+++ ehr-frontend-next/app/services/doctors_service.tsx
@@ -3,7 +3,6 @@ import axios from 'axios';
 // fetch all doctors. For fetching use docktor/all endpoint and pass the token in the header
 export async function fetchDoctors() {
     console.log("Fetching doctors...");
-    console.log("Token: ", localStorage.getItem('token'));
     const response = await axios.get('http://localhost:8085/doctor/all', {
 
         headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }
