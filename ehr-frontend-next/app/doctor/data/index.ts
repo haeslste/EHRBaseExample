07a4600fdb3838b2ae673396e1b6d9c9059064d3
Steven Haesler <hasv@zhaw.ch>
2025-04-23 10:47:11 +0200

    update doctor sidebar to include patient list and remove everything else

---
+++ ehr-frontend-next/app/doctor/data/index.ts
@@ -0,0 +1,33 @@
+import * as Icons from "../icons";
+
+export const NAV_DATA = [
+  {
+    label: "MAIN MENU",
+    items: [
+      {
+        title: "Dashboard",
+        icon: Icons.HomeIcon,
+        items: [
+          {
+            title: "Patients",
+            url: "/doctor/patients",
+          },
+        ],
+      },
+      {
+        title: "Forms",
+        icon: Icons.Alphabet,
+        items: [
+          {
+            title: "Form Elements",
+            url: "/forms/form-elements",
+          },
+          {
+            title: "Form Layout",
+            url: "/forms/form-layout",
+          },
+        ],
+      },
+    ],
+  },
+];
