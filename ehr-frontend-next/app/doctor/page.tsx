86930567e3055968c460c4362b7e3a9784458fd5
Steven Haesler <hasv@zhaw.ch>
2025-04-23 10:46:26 +0200

    add initial version of doctor dashboard

--- ehr-frontend-next/app/doctor/page.tsx
+++ ehr-frontend-next/app/doctor/page.tsx
@@ -1,4 +1,51 @@
-export default function DoctorDashboard() {
-    return <h1>Doctor Dashboard</h1>
+"use client";
+import { FC, useState, useEffect } from 'react';
+import { DashboardStatCard } from '@/components/cards/DashboardStatCard';
+import {fetchDoctors} from "@/services/doctors_service";
+import {fetchPatients} from "@/services/patients_service";
+import { fetchTemplates } from '@/services/template_service';
+
+interface UserData {
+  id: string;
+  firstName: string;
+  lastName: string;
+  username: string;
+  role: string;
+  user: { id: string; 
+    username:string;
   }
-  
+}
+
+const DoctorDashboard: React.FC = () => {
+  const [users, setUsers] = useState([{}]);
+  const [templates, setTemplates] = useState([{}]);
+  const [forms, setForms] = useState([]);
+  const [compositions, setCompositions] = useState([]); 
+  const [patients, setPatients] = useState([]);
+  const [doctors, setDoctors] = useState([]);
+
+  const fetchData = async () => {
+    const doctors = await fetchDoctors();
+    const patients = await fetchPatients();
+    const templates = await fetchTemplates();
+    setDoctors(doctors);
+    setPatients(patients);
+    setTemplates(templates);
+  }
+ 
+  useEffect(() => {
+      fetchData();
+  }, []);
+
+  return (
+    <div className="p-16 space-y-16">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+        <DashboardStatCard title="Patients" count={patients.length} />
+        <DashboardStatCard title="Forms" count={forms.length} />
+        <DashboardStatCard title="Compositions" count={forms.length} />
+      </div>
+    </div>
+  );
+};
+
+export default DoctorDashboard;
