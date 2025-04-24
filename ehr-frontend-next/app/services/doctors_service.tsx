536be21d98a2b136f42745a3ee50733bc87daa5e
Steven Haesler <hasv@zhaw.ch>
2025-04-24 10:56:09 +0200

    add doctor profile load based on user id

--- ehr-frontend-next/app/services/doctors_service.tsx
+++ ehr-frontend-next/app/services/doctors_service.tsx
@@ -11,3 +11,21 @@ export async function fetchDoctors() {
     return response.data;
 }
 
+
+export async function fetchMyDoctorProfile() {
+    console.log("Fetching current doctor profile...");
+    try {
+        if (localStorage.getItem('token') === null) {
+            console.warn("No token found in localStorage.");
+            return null;
+        }
+        const response = await axios.get('http://localhost:8085/doctor/me', {
+            headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }
+        });
+        console.log("My Doctor Profile Response: ", response.data);
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching doctor profile:", error);
+        return null;
+    }
+}
