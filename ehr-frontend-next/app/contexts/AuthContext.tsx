922c5412d23e79bfb42166c200da0b2fdbeb420d
Steven Haesler <hasv@zhaw.ch>
2025-04-24 11:01:56 +0200

    add id to auth context

--- ehr-frontend-next/app/contexts/AuthContext.tsx
+++ ehr-frontend-next/app/contexts/AuthContext.tsx
@@ -2,34 +2,35 @@
 import { createContext, useContext, useEffect, useState } from 'react';
 import Cookies from 'js-cookie';
 import { jwtDecode } from 'jwt-decode';
+import { userInfo } from 'os';
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [auth, setAuth] = useState({ token: null, role: null });
+  const [auth, setAuth] = useState({ token: null, role: null, userId: Number });
 
   useEffect(() => {
     const token = Cookies.get('token');
     if (token) {
       try {
         const decoded = jwtDecode(token);
-        setAuth({ token, role: decoded.role });
+        setAuth({ token, role: decoded.role, userId: decoded.userId });
       } catch (e) {
         console.error('Invalid token');
         Cookies.remove('token');
-        setAuth({ token: null, role: null });
+        setAuth({ token: null, role: null, userId: null });
       }
     }
   }, []);
 
-  const login = (token, role) => {
+  const login = (token, role, userId) => {
     Cookies.set('token', token, { expires: 7 }); // persist for 7 days
-    setAuth({ token, role });
+    setAuth({ token, role, userId: userId });
   };
 
   const logout = () => {
     Cookies.remove('token');
-    setAuth({ token: null, role: null });
+    setAuth({ token: null, role: null, userId: null });
   };
 
   return (
